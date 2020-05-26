ea97dd0eae03dce44f7011d18e16aa404b0f22f1
gtechzilla <okiomagerald@gmail.com>
2020-05-26 06:02:03 +0300

    has functions to process our tweets before they go to the database

---
+++ preprocessing_for_db.py
@@ -0,0 +1,17 @@
+import re
+
+#The fuctions are used to clean the tweets
+def clean_tweet(self, tweet): 
+    ''' 
+    Use sumple regex statemnents to clean tweet text by removing links and special characters
+    '''
+    return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t]) \
+                                |(\w+:\/\/\S+)", " ", tweet).split()) 
+def deEmojify(text):
+    '''
+    Strip all non-ASCII characters to remove emoji characters
+    '''
+    if text:
+        return text.encode('ascii', 'ignore').decode('ascii')
+    else:
+        return None
